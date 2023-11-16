dadc6753133d46737bbd076cf6052a5094b27eac
drewhouses <drewcasas@live.com>
2023-11-16 14:22:22 -0600

    added async/await to routes to allow it to correctly send back the json data from DB

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,25 +3,25 @@ const { Category, Product } = require("../../models");
 
 // The `/api/categories` endpoint
 
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
   // find all categories
   // be sure to include its associated Products
   try {
-    const categoryData = Category.findAll({
+    const categoryData = await Category.findAll({
       include: [{ model: Product }],
     });
+    console.log("You did it!");
     res.status(200).json(categoryData);
   } catch (err) {
-    console.log(err);
     res.status(500).json(err);
   }
 });
 
-router.get("/:id", (req, res) => {
+router.get("/:id", async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
   try {
-    const categoryData = Category.findByPk(req.params.id, {
+    const categoryData = await Category.findByPk(req.params.id, {
       include: [{ model: Product }],
     });
 
